ca5418109e3cca7215d49bcc48a722149c6f767f
Razor <74922874+RazorOnDev@users.noreply.github.com>
2021-08-30 12:58:14 +0200

    Added Code to the main Script.

--- script.cs
+++ script.cs
@@ -1,1 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
+public class script : MonoBehaviour
+{
+
+    public float VarSumada;
+
+    public float ResultadoTotal;
+
+    public Text VarSumadaDisplay;
+
+    private void Start()
+    {
+        VarSumada = PlayerPrefs.GetFloat("VarSumada", VarSumada);
+    }
+    public void Update()
+    {
+        VarSumadaDisplay.text = "" + Mathf.Round(VarSumada);
+    }
+    public void ClickChocolate()
+    {
+        VarSumada += 1;
+        PlayerPrefs.SetFloat("VarSumada", VarSumada);
+    }
+}
